1f481e0f5798847833726b88f8fc2e21c7cebead
Davi Lisboa <davifranca135@gmail.com>
2026-01-27 23:33:15 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,18 @@ public class Main {
     private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()_+-=[]|,./?><";
 
     public static String generatePassword(int length) {
-        
+        SecureRandom randomNumberGenerator = new SecureRandom();
+        StringBuilder password = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = randomNumberGenerator.nextInt(CHARACTERS.length());
+            password.append(CHARACTERS.charAt(index));
+        }
+
+        return password.toString();
     }
 
     public static void main(String[] args) {
+        
     }
 }
